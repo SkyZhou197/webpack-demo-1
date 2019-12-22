9aa96c9789efa7469b85d9faf2c70f512275cb66
skyzhou <zhouhaomin019@gmail.com>
2019-12-22 15:57:18 +0800

    准备上传了

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
 import x from "./x.js";
-console.log("hi");
+import jpg from "./assets/1.jpg";
+console.log(jpg);
+
+const div = document.getElementById("app");
+console.log("div");
+console.log(div);
+div.innerHTML = `
+  <img src="${jpg}">`;
+
+const button = document.createElement("button");
+button.innerHTML = "懒加载";
+button.onclick = () => {
+  const promise = import("./lazy");
+  promise.then(
+    module => {
+      const fn = module.default;
+      fn();
+    },
+    () => {
+      console.log("模块加载错误");
+    }
+  );
+};
+
+div.appendChild(button);
